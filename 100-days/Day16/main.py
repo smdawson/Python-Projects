af58831affd5b159699a95da09b690c7acbbfb73
smdawson <seth.dawson@hotmail.com>
2022-07-05 19:41:19 -0500

    Delete 100-days directory

--- 100-days/Day16/main.py
+++
@@ -1,5 +0,0 @@
-import another_module
-print(another_module.another_variable)
-
-
-import turtle
--- 100-days/Day26/main.py
+++
@@ -1,7 +0,0 @@
-# numbers = [1, 2, 3]
-# new_numbers = [n + 1 for n in numbers]
-# name = "Seth"
-# letters_list = [letter for letter in name]
-
-nums = range(1, 5)
-print(nums)
